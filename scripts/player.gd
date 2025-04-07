46d9a92508cdc3c9158ebbc0d430c16a69fc3256
jesspancakes <c24453362@mytudublin.ie>
2025-04-07 13:27:42 +0100

    player can MOVE and the cameraFOLLOWS IT

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,4 +1,18 @@
 extends CharacterBody2D
 
+var speed = 300
+
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+
+func _physics_process(delta: float) -> void:
+	var move_dir = Vector2(Input.get_axis("move_left", "move_right"), Input.get_axis("move_up", "move_down"))
+	
+	if move_dir != Vector2.ZERO:
+		velocity = speed * move_dir.normalized()
+	else: 
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+	
+	
+	move_and_slide()
