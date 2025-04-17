606fc3621a4d0ff35827a6f854b645165484b79d
jesspancakes <c24453362@mytudublin.ie>
2025-04-17 13:21:56 +0100

    enemy follows and kills the player

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -3,13 +3,26 @@ class_name Enemy
 
 var player: Player = null 
 
-func _process(delta: float) -> void:
-	pass
-	
+var speed: float = 100
+var direction := Vector2.ZERO
 
+func _process(delta: float) -> void:
+	if player != null:
+		look_at(player.global_position)
 
 func _physics_process(delta: float) -> void:
-	pass
+	if player != null:
+		var enemy_to_player = (player.global_position - global_position)
+		direction = enemy_to_player.normalized()
+		
+		if direction != Vector2.ZERO:
+			velocity = speed * direction.normalized()
+		else: 
+			velocity.x = move_toward(velocity.x, 0, speed)
+			velocity.y = move_toward(velocity.y, 0, speed)
+		
+		
+		move_and_slide()
 
 func _on_player_detector_body_entered(body: Node2D) -> void:
 	if body is Player:
