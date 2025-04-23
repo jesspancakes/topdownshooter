8d8b3761a9c60823943c9e226f6c89ed64d498d4
jesspancakes <c24453362@mytudublin.ie>
2025-04-23 18:15:49 +0100

    the bullet shoots behind the player AAAAAAAAAAAAA

--- snowman2/Player1.gd
+++ snowman2/Player1.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const bulletPath = preload("res://Bullet.tscn")
 
+var speed = 300
 
 func _ready():
 	pass
@@ -19,6 +20,18 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	
+func _physics_process(delta: float) -> void:
+	var move_dir = Vector2(Input.get_axis("ui_left", "ui_right"), Input.get_axis("ui_up", "ui_down"))
+	
+	if move_dir != Vector2.ZERO:
+		velocity = speed * move_dir.normalized()
+	else: 
+		velocity.x = move_toward(velocity.x, 0, speed)
+		velocity.y = move_toward(velocity.y, 0, speed)
+	
+	
+	move_and_slide()
+
 
 
 
@@ -29,3 +42,4 @@ func shoot():
 	bullet.position = $Node2D/Marker2D.global_position
 	
 	bullet.velocity = get_global_mouse_position() - bullet.position
+	
