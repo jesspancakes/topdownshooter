dd897b4385c3f95663992b9e74e053cdf2092b27
jesspancakes <c24453362@mytudublin.ie>
2025-05-02 12:35:16 +0100

    start and lose screen

--- snowman2/title_screen.gd
+++ snowman2/title_screen.gd
@@ -3,3 +3,8 @@ extends Control
 func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")
 	pass # Replace with function body.
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
+	pass # Replace with function body.
