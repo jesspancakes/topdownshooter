0dc4503f265ad5d1b3e1cac3d1caa129b4b1227e
jesspancakes <c24453362@mytudublin.ie>
2025-05-07 21:03:19 +0100

    dont know how to add score :(((

--- snowman2/game_over_screen.gd
+++ snowman2/game_over_screen.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 	pass # Replace with function body.
--- snowman2/main.gd
+++ snowman2/main.gd
@@ -1,5 +1,27 @@
 extends Node
 
+var friends : int
+var max_enemies : int
 
-func _on_enemy_spawner_hit_p() -> void:
-	pass
+func _ready():
+	max_enemies = 10
+	friends = 0
+	$Player2/HUD/Friends.text = "Friends : " + str(friends)
+	
+func _process(delta):
+	if is_wave_completed():
+		get_tree().change_scene_to_file("res://win.tscn")
+
+func _on_enemy_spawner_hit_p():
+	get_tree().change_scene_to_file("res://game_over_screen.tscn")
+
+func is_wave_completed():
+	var all_dead = true
+	var enemies = get_tree().get_first_node_in_group("enemies")
+	if enemies.size() == max_enemies:
+		for e in enemies:
+			if e.alive:
+				all_dead = false
+		return all_dead
+	else:
+		return false
